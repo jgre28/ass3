91af9356df46e4d83fb0ebbd1c3809bd61b19685
jgre28 <jgre28@student.monash.edu>
2018-10-08 11:12:33 +1100

    started project and added database connection details

---
+++ connection.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jordan
+ * Date: 8/10/2018
+ * Time: 11:11 AM
+ */
+
+$HOST = "130.194.7.82";
+$UName = "s25099159";
+$PWord = "monash00";
+$DB = "s25099159";
+?>
